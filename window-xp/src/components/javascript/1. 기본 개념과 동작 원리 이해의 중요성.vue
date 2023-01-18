56592aef6a7b9288fc64fefaa44cc0c25dd0346e
qoalstjd <bms980805@gmail.com>
2023-01-18 16:38:03 +0900

    v2.0.0 vue.js 로 변경

---
+++ window-xp/src/components/javascript/1. 기본 개념과 동작 원리 이해의 중요성.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
