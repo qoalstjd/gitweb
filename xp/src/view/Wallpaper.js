940a723b647821faadca2bc0cfd6afba7fcd4892
qoalstjd <bms980805@gmail.com>
2023-02-06 15:18:51 +0900

    .

--- xp/src/view/Wallpaper.js
+++ xp/src/view/Wallpaper.js
@@ -1,16 +1,22 @@
 import { useState, useEffect } from "react";
-import { collection, addDoc, getDocs, query, onSnapshot, orderBy } from "firebase/firestore";
+import { collection, addDoc, getDocs, query, onSnapshot, orderBy, where } from "firebase/firestore";
 import { dbService } from "../fb";
 import styled from "styled-components";
 
 import Directory from "../components/Directory";
+import TaskBar from "../components/TaskBar";
 
 const StyledWallpaper = styled.section`
   background: url(img/wallpaper.jpg) no-repeat center;
   background-size: cover;
+  height: calc(100vh - 36px);
 
   ul {
     position: absolute;
+  }
+
+  ul > li > button.on + div {
+    display: block;
   }
 `;
 
@@ -37,12 +43,18 @@ const Wallpaper = () => {
     document.addEventListener("click", onClearContextMenu, false);
   }, []);
 
+  const today = new Date();
   // 파일생성 이벤트
   const onFileCreate = async () => {
     try {
-      const docRef = await addDoc(collection(dbService, "JavaScript"), {
-        name: "abc",
-        content: "lorem lisum",
+      const docRef = await addDoc(collection(dbService, "React"), {
+        title: "리액트의 이해",
+        content: "lorem lisum apooijet",
+        createdAt: Date.now(),
+        createdDate: `${today.getFullYear()}-${today.getMonth() >= 10 ? today.getMonth() : "0" + today.getMonth()}-${
+          today.getDate() >= 10 ? today.getDate() : "0" + today.getDate()
+        }`,
+        type: "JavaScript",
       });
     } catch (e) {
       console.error("Error adding document: ", e);
@@ -50,12 +62,9 @@ const Wallpaper = () => {
   };
 
   // 파일 가져오기
-  const [files, setFiles] = useState();
+  const [files, setFiles] = useState([]);
   useEffect(() => {
-    const q = query(
-      collection(dbService, "JavaScript"),
-      // orderBy("createdAt", "desc"),
-    );
+    const q = query(collection(dbService, "React"));
     onSnapshot(q, snapshot => {
       const fileArr = snapshot.docs.map(document => ({
         id: document.id,
@@ -64,6 +73,23 @@ const Wallpaper = () => {
       setFiles(fileArr);
     });
   }, []);
+  const [dirFiles, setDirFiles] = useState([]);
+  useEffect(() => {
+    const q = query(collection(dbService, "files"), where("type", "==", "JavaScript"), orderBy("createdAt", "desc"));
+    onSnapshot(q, snapshot => {
+      const dirFileArr = snapshot.docs.map(document => ({
+        id: document.id,
+        ...document.data(),
+      }));
+      setDirFiles(dirFileArr);
+    });
+  }, []);
+
+  // 디렉토리 열기
+  const [openedDir, setOpenedDir] = useState([]);
+  const dirOpen = event => {
+    if (!openedDir.includes(event.target.innerHTML)) setOpenedDir([...openedDir, event.target.innerHTML]);
+  };
 
   return (
     <>
@@ -75,14 +101,17 @@ const Wallpaper = () => {
         </StyledContext>
         <ul>
           <li>
-            <button>JavaScript</button>
+            <button onClick={dirOpen}>JavaScript</button>
+            {files.map(fileObj => (
+              <Directory key={fileObj.id} fileObj={files} />
+            ))}
           </li>
           <li>
-            <button>React</button>
+            <button onClick={dirOpen}>React</button>
           </li>
         </ul>
-        <div className="taskBar"></div>
       </StyledWallpaper>
+      <TaskBar openedDir={openedDir} />
     </>
   );
 };
